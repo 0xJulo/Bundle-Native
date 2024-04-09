153322a80ab501a0ce9dfb84bbaa1e583730a3f5
Julo <9354218+0xJulo@users.noreply.github.com>
2024-04-09 19:14:12 +0100

    edit tab functionality to render tabs based on condition

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import Bundle, { BundleProps } from './BundleComponent';
+
+import { useExampleData } from '../ExampleDataStore';
+
+interface TabsProps {
+    items: BundleProps[];
+};
 
 const Tabs: React.FC = () => {
-    const [activeTab, setActiveTab] = useState('favourites');
+    const { items } = useExampleData();
+    const [activeTab, setActiveTab] = useState('all');
+    const activeBundles = items.filter(item => 
+        activeTab.toLowerCase() === 'all' || item.type.toLowerCase() === activeTab.toLowerCase()
+    );
 
     const activeTabContent = () => {
         switch (activeTab) {
@@ -36,30 +47,46 @@ const Tabs: React.FC = () => {
     return (
         <>
             <View style={styles.container}>
+                <View style={styles.tabContainer}>
                 <TouchableOpacity
-                    style={styles.tab}
+                    style={[styles.tab, activeTab === 'all' && styles.activeTab]}
                     onPress={() => setActiveTab('all')}
                 >
-                    <Text>All Bundles</Text>
+                    <Text style={[styles.text, activeTab === 'all' && styles.activeText]}>All Bundles</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    style={styles.tab}
-                    onPress={() => setActiveTab('saved')}
+                    style={[styles.tab, activeTab === 'Saved' && styles.activeTab]}
+                    onPress={() => setActiveTab('Saved')}
                 >
-                    <Text>Saved</Text>
+                    <Text style={[styles.text, activeTab === 'Saved' && styles.activeText]}>Saved</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    style={styles.tab}
-                    onPress={() => setActiveTab('myBundles')}
+                    style={[styles.tab, activeTab === 'My Bundles' && styles.activeTab]}
+                    onPress={() => setActiveTab('My Bundles')}
                 >
-                    <Text>My Bundles</Text>
+                    <Text style={[styles.text, activeTab === 'My Bundles' && styles.activeText]}>My Bundles</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    style={styles.tab}
-                    onPress={() => setActiveTab('popular')}
+                    style={[styles.tab, activeTab === 'Popular' && styles.activeTab]}
+                    onPress={() => setActiveTab('Popular')}
                 >
-                    <Text>Popular</Text>
+                    <Text style={[styles.text, activeTab === 'Popular' && styles.activeText]}>Popular</Text>
                 </TouchableOpacity>
+                </View>
+                <View>
+                {activeBundles.map((bundle, index) => (
+                    <Bundle 
+                        key={bundle.id} 
+                        id={bundle.id} 
+                        name={bundle.name} 
+                        description={bundle.description} 
+                        created={bundle.created} 
+                        type={bundle.type} 
+                        steps={bundle.steps} 
+                        owner={bundle.owner} 
+                        tags={bundle.tags} />
+                ))}
+                </View>
             </View>
         </>
     );
@@ -67,16 +94,33 @@ const Tabs: React.FC = () => {
 
 const styles = StyleSheet.create({
     container: {
+        flexDirection: 'column',
+        marginBottom: 15,
+    },
+    tabContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
         padding: 10,
-        backgroundColor: 'gray',
         width: '100%',
         marginBottom: 15,
     },
     tab: {
-        paddingVertical: 10,
+        paddingVertical: 15,
         paddingHorizontal: 20,
+        backgroundColor: '#192d32',
+        borderColor: '#80baa8',
+        //borderBottomWidth: 1,
+    },
+    activeTab: {
+        backgroundColor: '#80baa8',
+    },
+    text: {
+        color: '#80baa8',
+        textDecorationLine: 'underline',
+    },
+    activeText: {
+        color: '#192d32',
+        textDecorationLine: 'none',
     },
     content: {
         padding: 20,
