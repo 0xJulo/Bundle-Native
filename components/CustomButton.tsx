a8dd2cea0918f4a5739e07afe089011a29503598
Julo <9354218+0xJulo@users.noreply.github.com>
2024-04-09 13:07:47 +0100

    Custom button can receive an icon

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 // React Native imports
 import { Pressable, Text, StyleSheet } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 // Interface props
 interface CustomButtonProps {
     text: string,
+    icon?: string,
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ text }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ text, icon }) => {
     return (
-        <Pressable style={styles.button}>
+        <Pressable style={icon ? styles.buttonIcon : styles.button}>
             <Text style={styles.buttonText}>{text}</Text>
+            {icon && <MaterialIcons name={icon} size={30} color='#192d32' />}
         </Pressable>
     );
 };
@@ -23,8 +26,20 @@ const styles = StyleSheet.create({
         height: 50,
         marginTop: 10,
         borderRadius: 10,
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    buttonIcon: {
+        backgroundColor: '#80baa8',
+        width: '100%',
+        height: 50,
+        marginTop: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
     },
     buttonText: {
         color: '#192d32',
